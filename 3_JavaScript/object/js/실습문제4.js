1afed26bc57e13f30f747deacfb82ce3bf1bfba6
yogu970126 <yogu970126@gmail.com>
2023-12-18 17:47:18 +0900

    js 실습문제4 내 풀이
    다 못 풀었음

---
+++ 3_JavaScript/object/js/실습문제4.js
@@ -0,0 +1,12 @@
+const btnFinish = document.querySelector("#finish");
+const waring = document.querySelector("#waring");
+let id = /^([A-Z]|[a-z])([A-Z]|[a-z]|[0-9])$/;
+
+btnFinish.addEventListener("click", function () {
+  if (id.test(userId.value)) {
+    alert("정상 입력");
+  } else {
+    alert("잘못 입력");
+    waring.setAttribute("style", "red");
+  }
+});
