bc23dfdc823e68a43808311c44e8c578aa012850
yogu970126 <yogu970126@gmail.com>
2023-12-19 23:20:57 +0900

    연습3
    시험대비 폼입력 연습
    - 정규표현식

--- practice/js/연습3.js
+++ practice/js/연습3.js
@@ -1,1 +1,17 @@
-const name = document.querySelector(".userName");
+const finish = document.querySelector("#finish");
+let id = /^([A-Z]|[a-z])([A-Z]|[a-z]|[0-9]){3,11}$/;
+let name = /^[가-힣]{2,}$/;
+let password = /[a-zA-Z0-9]{4,10}/;
+
+finish.addEventListener("click", function () {
+  if (id.test(userId.value)) {
+    alert("아이디 OK!");
+  } else {
+    alert("잘못 입력");
+  }
+  if (name.test(userName.value)) {
+    alert("이름 OK!");
+  } else {
+    alert("잘못 입력");
+  }
+});
