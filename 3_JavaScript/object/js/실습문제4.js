8fbeddd31c2355ba2a6ada36dfc395d4295a5bd0
yogu970126 <yogu970126@gmail.com>
2023-12-19 17:48:50 +0900

    실습문제4
    정규표현식 숫자 범위 조정

--- 3_JavaScript/object/js/실습문제4.js
+++ 3_JavaScript/object/js/실습문제4.js
@@ -1,6 +1,6 @@
 const btnFinish = document.querySelector("#finish");
 const waring = document.querySelector("#waring");
-let id = /^([A-Z]|[a-z])([A-Z]|[a-z]|[0-9])$/;
+let id = /^([A-Z]|[a-z])([A-Z]|[a-z]|[0-9]){3,11}$/;
 
 btnFinish.addEventListener("click", function () {
   if (id.test(userId.value)) {
