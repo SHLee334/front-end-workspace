d5af6d48fa426938935b2585941d4e2ed1d14ccb
yogu970126 <yogu970126@gmail.com>
2024-04-14 16:29:08 +0900

    리액트 5_async
    리액트 5_async
    - 비동

--- 5_React/5-async/src/App.js
+++ 5_React/5-async/src/App.js
@@ -1,9 +1,38 @@
-import { useEffect } from "react";
-import { getFetch } from "./api/animal";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { getFetch, getAwait, getAxios, useGetQuery } from "./api/animal";
 
 const App = () => {
-  return <></>;
+  // const [data, setData] = useState([]);
+
+  // const dataLoad = async () => {
+  //   const result = await getAxios();
+  //   setData(result);
+  //   // getAwait().then((result) => {
+  //   //   console.log(result);
+  //   //   setData(result);
+  //   // });
+  // };
+
+  // useEffect(() => {
+  //   dataLoad();
+  // }, []);
+
+  // react-query
+  const { data, isLodaing, isError } = useGetQuery();
+
+  // 로딩중일때 보여줄 화면
+  if (isLodaing) return <>Loading...</>;
+
+  // 에러 났을때 보여줄 화면
+  if (isError) return <>Error...</>;
+
+  return (
+    <>
+      {data?.map((animal) => (
+        <div key={animal.no}>{animal.name}</div>
+      ))}
+    </>
+  );
 };
 
 export default App;
--- 5_React/5-async/src/api/animal.js
+++ 5_React/5-async/src/api/animal.js
@@ -1,5 +1,35 @@
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+
+// fetch ~ then
 export const getFetch = () => {
-  return fetch("http://localhost:8080/api/animal").then((response) => {
-    console.log(response);
+  return fetch("http://localhost:8080/api/animal")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      return data;
+    });
+};
+
+// async ~ await
+export const getAwait = async () => {
+  const response = await fetch("http://localhost:8080/api/animal");
+  console.log(response);
+  return response.json();
+};
+
+// axios (async ~ await)
+export const getAxios = async () => {
+  const response = await axios.get("http://localhost:8080/api/animal");
+  console.log(response);
+  return response.data;
+};
+
+// react-query
+export const useGetQuery = () => {
+  return useQuery({
+    queryKey: ["animal"],
+    queryFn: getAxios,
   });
 };
