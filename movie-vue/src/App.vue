6710fc34b3ab3c13c07bbba13e1833ffb7968d60
SHLee334 <yogu970126@gmail.com>
2024-04-19 17:51:38 +0900

    Vue (뷰)
    Vue (뷰)
    - 4/19 시험내용 뷰로 구현

---
+++ movie-vue/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <router-view name="header" />
+  <router-view />
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  // beforeCreate -> created -> beforeMount -> mounted
+  // beforeUpdate -> updated
+  // beforeUnmount -> unmounted << vue3 기준 / beforeDestroy -> destroyed << vue2 기준
+  // created, mounted, updated 가 많이 쓰인다
+};
+</script>
+
+<style></style>
